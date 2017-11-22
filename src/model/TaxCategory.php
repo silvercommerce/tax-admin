b7fcb08f614a8db65e1dadcbfbbf442d5dd2bb9c
Morven Lewis-Everley <morven@i-lateral.com>
2017-11-22 20:56:50 +0000

    Add default model classes

---
+++ src/model/TaxCategory.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace SilverCommerce\TaxAdmin\Model;
+
+use SilverStripe\ORM\DataObject;
+use SilverStripe\ORM\DB;
+use SilverStripe\Forms\RequiredFields;
+use SilverStripe\Security\PermissionProvider;
+use SilverStripe\Security\Permission;
+use SilverStripe\Security\Member;
+use SilverStripe\SiteConfig\SiteConfig;
+
+/**
+ * A tax rate can be added to a product and allows you to map a product
+ * to a percentage of tax.
+ * 
+ * If added to a product, the tax will then be added to the price
+ * automatically. 
+ * 
+ * @author i-lateral (http://www.i-lateral.com)
+ * @package catalogue
+ */
+class TaxCategory extends DataObject implements PermissionProvider
+{
+    
+    private static $table_name = 'TaxCategory';
+
+    private static $db = [
+        "Title" => "Varchar"
+    ];
+    
+    private static $has_one = [
+        "Site" => SiteConfig::class
+    ];
+
+    private static $many_many = [
+        "Rates" => TaxRate::class
+    ];
+
+    private static $many_many_extrafields = [
+        "Rates" => [
+            "Location" => "Enum(array('Shipping','Billing','Store'), 'Shipping')"
+        ]
+    ];
+
+    private static $summary_fields = [
+        "Title",
+        "RatesList"
+    ];
+
+    private static $casting = [
+        "RatesList" => "Varchar(255)"
+    ];
+    
+    public function getCMSValidator()
+    {
+        return RequiredFields::create([
+            "Title"
+        ]);
+    }
+
+    public function getRatesList()
+    {
+        return implode(", ", $this->Rates()->column("Title"));
+    }
+    
+    public function requireDefaultRecords()
+    {
+        
+        // If no tax rates, setup some defaults
+        if (!TaxCategory::get()->exists()) {
+            $config = SiteConfig::current_site_config();
+
+            $cat = TaxCategory::create([
+                "Title" => "Standard Goods"
+            ]);
+            $cat->SiteID = $config->ID;
+            $cat->write();
+
+            DB::alteration_message(
+                'Standard Goods tax category created',
+                'created'
+            );
+        }
+        
+        parent::requireDefaultRecords();
+    }
+
+    public function providePermissions()
+    {
+        return [
+            "TAXADMIN_MANAGE_CATEGORY" => [
+                'name' => 'Manage Tax Categories',
+                'help' => 'Allow user to create, edit and delete tax categoires',
+                'category' => 'Tax',
+                'sort' => 10
+            ]
+        ];
+    }
+
+    /**
+     * Anyone can view tax categories
+     *
+     * @param Member $member
+     * @return boolean
+     */
+    public function canView($member = null)
+    {
+        $extended = $this->extendedCan(__FUNCTION__, $member);
+        
+        if ($extended !== null) {
+            return $extended;
+        }
+
+        return true;
+    }
+
+    /**
+     * Anyone can create orders, even guest users
+     *
+     * @param Member $member
+     * @return boolean
+     */
+    public function canCreate($member = null, $context = [])
+    {
+        $extended = $this->extendedCan(__FUNCTION__, $member, $context);
+        
+        if ($extended !== null) {
+            return $extended;
+        }
+        
+        if (!$member) {
+            $member = Member::currentUser();
+        }
+
+        if ($member && Permission::checkMember($member->ID, ["ADMIN", "TAXADMIN_MANAGE_CATEGORY"])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Only users with correct rights can edit
+     *
+     * @param Member $member
+     * @return boolean
+     */
+    public function canEdit($member = null, $context = [])
+    {
+        $extended = $this->extendedCan(__FUNCTION__, $member);
+        
+        if ($extended !== null) {
+            return $extended;
+        }
+        
+        if (!$member) {
+            $member = Member::currentUser();
+        }
+
+        if ($member && Permission::checkMember($member->ID, ["ADMIN", "TAXADMIN_MANAGE_CATEGORY"])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * No one should be able to delete an order once it has been created
+     *
+     * @param Member $member
+     * @return boolean
+     */
+    public function canDelete($member = null, $context = [])
+    {
+        $extended = $this->extendedCan(__FUNCTION__, $member);
+        
+        if ($extended !== null) {
+            return $extended;
+        }
+        
+        if (!$member) {
+            $member = Member::currentUser();
+        }
+
+        if ($member && Permission::checkMember($member->ID, ["ADMIN", "TAXADMIN_MANAGE_CATEGORY"])) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/model/TaxRate.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace SilverCommerce\TaxAdmin\Model;
+
+use SilverStripe\ORM\DataObject;
+use SilverStripe\ORM\DB;
+use SilverStripe\Forms\RequiredFields;
+use SilverStripe\Security\PermissionProvider;
+use SilverStripe\Security\Permission;
+use SilverStripe\Security\Member;
+use SilverStripe\SiteConfig\SiteConfig;
+
+/**
+ * A tax rate can be added to a product and allows you to map a product
+ * to a percentage of tax.
+ * 
+ * If added to a product, the tax will then be added to the price
+ * automatically. 
+ * 
+ * @author i-lateral (http://www.i-lateral.com)
+ * @package catalogue
+ */
+class TaxRate extends DataObject implements PermissionProvider
+{
+    
+    private static $table_name = 'TaxRate';
+
+    private static $db = [
+        "Title" => "Varchar",
+        "Rate"=> "Decimal"
+    ];
+
+    private static $has_one = [
+        "Site" => SiteConfig::class
+    ];
+
+    private static $summary_fields = [
+        "Title",
+        "Rate"
+    ];
+    
+    public function getCMSValidator()
+    {
+        return RequiredFields::create([
+            "Title",
+            "Rate"
+        ]);
+    }
+    
+    public function requireDefaultRecords()
+    {
+        
+        // If no tax rates, setup some defaults
+        if (!TaxRate::get()->exists()) {
+            $config = SiteConfig::current_site_config();
+
+            $category = $config->TaxCategories()->first();
+
+            $vat = TaxRate::create();
+            $vat->Title = "VAT";
+            $vat->Rate = 20;
+            $vat->SiteID = $config->ID;
+            $vat->write();
+            DB::alteration_message(
+                'VAT tax rate created.',
+                'created'
+            );
+            
+            $reduced = TaxRate::create();
+            $reduced->Title = "Reduced rate";
+            $reduced->Rate = 5;
+            $reduced->SiteID = $config->ID;
+            $reduced->write();
+            DB::alteration_message(
+                'Reduced tax rate created.',
+                'created'
+            );
+            
+            $zero = TaxRate::create();
+            $zero->Title = "Zero rate";
+            $zero->Rate = 0;
+            $zero->SiteID = $config->ID;
+            $zero->write();
+            DB::alteration_message(
+                'Zero tax rate created.',
+                'created'
+            );
+
+            if ($category) {
+                $category->Rates()->add($vat);
+                $category->Rates()->add($reduced);
+                $category->Rates()->add($zero);
+                DB::alteration_message(
+                    'Added tax to category',
+                    'created'
+                );
+                
+            }
+        }
+        
+        parent::requireDefaultRecords();
+    }
+
+    public function providePermissions()
+    {
+        return [
+            "TAXADMIN_MANAGE_RATE" => [
+                'name' => 'Manage Tax Rates',
+                'help' => 'Allow user to create, edit and delete tax rates',
+                'category' => 'Tax',
+                'sort' => 0
+            ]
+        ];
+    }
+
+    /**
+     * Anyone can view tax categories
+     *
+     * @param Member $member
+     * @return boolean
+     */
+    public function canView($member = null)
+    {
+        $extended = $this->extendedCan(__FUNCTION__, $member);
+        
+        if ($extended !== null) {
+            return $extended;
+        }
+
+        return true;
+    }
+
+    /**
+     * Anyone can create orders, even guest users
+     *
+     * @param Member $member
+     * @return boolean
+     */
+    public function canCreate($member = null, $context = [])
+    {
+        $extended = $this->extendedCan(__FUNCTION__, $member, $context);
+        
+        if ($extended !== null) {
+            return $extended;
+        }
+
+        if (!$member) {
+            $member = Member::currentUser();
+        }
+
+        if ($member && Permission::checkMember($member->ID, ["ADMIN", "TAXADMIN_MANAGE_RATE"])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Only users with correct rights can edit
+     *
+     * @param Member $member
+     * @return boolean
+     */
+    public function canEdit($member = null, $context = [])
+    {
+        $extended = $this->extendedCan(__FUNCTION__, $member);
+        
+        if ($extended !== null) {
+            return $extended;
+        }
+
+        if (!$member) {
+            $member = Member::currentUser();
+        }
+
+        if ($member && Permission::checkMember($member->ID, ["ADMIN", "TAXADMIN_MANAGE_RATE"])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * No one should be able to delete an order once it has been created
+     *
+     * @param Member $member
+     * @return boolean
+     */
+    public function canDelete($member = null, $context = [])
+    {
+        $extended = $this->extendedCan(__FUNCTION__, $member);
+        
+        if ($extended !== null) {
+            return $extended;
+        }
+
+        if (!$member) {
+            $member = Member::currentUser();
+        }
+
+        if ($member && Permission::checkMember($member->ID, ["ADMIN", "TAXADMIN_MANAGE_RATE"])) {
+            return true;
+        }
+
+        return false;
+    }
+}
