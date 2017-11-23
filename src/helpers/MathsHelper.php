c811cc283230804e0c62f5b4ba8bab85a8c84b66
Morven Lewis-Everley <morven@i-lateral.com>
2017-11-23 15:20:04 +0000

    Add maths helper utility class

---
+++ src/helpers/MathsHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SilverCommerce\TaxAdmin\Helpers;
+
+/**
+ * Simple helper class to provide generic functions to help with
+ * maths functions
+ */
+class MathsHelper
+{
+    /**
+     * Rounds up a float to a specified number of decimal places
+     * (basically acts like ceil() but allows for decimal places)
+     *
+     * @param float $value Float to round up
+     * @param int $places the number of decimal places to round to
+     * @return float
+     */
+    public static function round_up($value, $places = 0)
+    {
+        $offset = 0.5;
+
+        if ($places !== 0) {
+            $offset /= pow(10, $places);
+        }
+        
+        $return = round(
+            $value + $offset,
+            $places,
+            PHP_ROUND_HALF_DOWN
+        );
+        
+        return $return;
+    }
+}
